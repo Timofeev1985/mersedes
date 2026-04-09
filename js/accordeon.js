a6be0231ec76c9d2622c49a43203d355dd2bfd74
Timofeev1985 <timon_1985_@mail.ru>
2026-04-09 09:37:44 +0300

    first commit

---
+++ js/accordeon.js
@@ -0,0 +1,18 @@
+const lists = document.querySelectorAll(".feature-sub");
+const btns = document.querySelectorAll(".feature__link");
+console.log(btns[1]);
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", () => console.log(btns[i]));
+// }
+btns.forEach((btnItem, index) => {
+  btnItem.addEventListener("click", () => {
+    btns.forEach((btnItem) => {
+      btnItem.classList.toggle("feature__link_active");
+    });
+    btnItem.classList.add("feature__link_active");
+    lists.forEach((listItem) => {
+      listItem.classList.add("hidden");
+    });
+    lists[index].classList.remove("hidden");
+  });
+});
